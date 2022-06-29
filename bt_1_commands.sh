cbc13338e94add92751f173fed0523ebed4f6039
project237 <berkozer12@gmail.com>
2022-06-29 14:03:21 +0300

    Completed the first set of commands (hopefully)

--- bt_1_commands.sh
+++ bt_1_commands.sh
@@ -1,6 +1,6 @@
 # test download code
-freqtrade download-data --exchange binance
+freqtrade download-data --exchange binance --pairs ETH/USDT
 
 # target download code
-freqtrade download-data -t 30m --timerange 20180401- --include-inactive-pairs --exchange binance
-freqtrade download-data -t 15m --timerange 20180401- --include-inactive-pairs --exchange binance
+freqtrade download-data -t 30m --timerange 20180401- --include-inactive-pairs --exchange binance --pairs ZEC/USDT
+freqtrade download-data -t 15m --timerange 20180401- --include-inactive-pairs --exchange binance --pairs ZEC/USDT
